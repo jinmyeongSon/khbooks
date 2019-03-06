fb8777b0b03ede570e584181c1b9946bbdf96d45
user2 <user2@KH_G>
2019-03-06 19:46:57 +0900

    dao controller service package 만듬

--- khbooks/src/main/java/kr/mycompany/khbook/HomeController.java
+++ khbooks/src/main/java/kr/mycompany/khbook/HomeController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+
+
 @Controller
 public class HomeController {
 	
--- khbooks/src/main/java/test/Test.java
+++ khbooks/src/main/java/test/Test.java
@@ -3,11 +3,18 @@ package test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//http://localhost:8090/khbook/index.kh
+
 @Controller
 public class Test {
 	@RequestMapping("index.kh")
 	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping("login.kh")
+	public String user() {
+		return "user/page-double-sidebar2";
 	}
 
 }
