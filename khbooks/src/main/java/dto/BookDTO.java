4ca4a15f6364291ddb9af9aca66e162e62f275f5
junhan <nagil8155@gmail.com>
2019-03-07 16:53:13 +0900

    DTO 업데이트

--- khbooks/src/main/java/dto/BookDTO.java
+++ khbooks/src/main/java/dto/BookDTO.java
@@ -1,4 +1,14 @@
 package dto;
 
+import java.util.Date;
+
 public class BookDTO {
+	int bno;
+	String bname;
+	int gno;
+	Date bupdate;
+	int bgrade;
+	int bview;
+	String bthumb;
+	String binfo;
 }
---
+++ khbooks/src/main/java/dto/SerialDTO.java
@@ -0,0 +1,16 @@
+package dto;
+
+import java.util.Date;
+
+public class SerialDTO {
+	int upno;
+	int bno;
+	Date sdate;
+	String scontent;
+	int sprice;
+	int ncount;
+	int auno;
+	String stitle;
+	int sgrade;
+
+}
