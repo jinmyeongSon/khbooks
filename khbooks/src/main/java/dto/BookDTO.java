b983573130a826cd09019dc141ae855ae3323e4b
junhan <nagil8155@gmail.com>
2019-03-07 16:58:20 +0900

    DTO 업데이트

--- khbooks/src/main/java/dto/BookDTO.java
+++ khbooks/src/main/java/dto/BookDTO.java
@@ -1,4 +1,64 @@
 package dto;
 
+import java.util.Date;
+
 public class BookDTO {
+	int bno;
+	String bname;
+	int gno;
+	Date bupdate;
+	int bgrade;
+	int bview;
+	String bthumb;
+	String binfo;
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getBname() {
+		return bname;
+	}
+	public void setBname(String bname) {
+		this.bname = bname;
+	}
+	public int getGno() {
+		return gno;
+	}
+	public void setGno(int gno) {
+		this.gno = gno;
+	}
+	public Date getBupdate() {
+		return bupdate;
+	}
+	public void setBupdate(Date bupdate) {
+		this.bupdate = bupdate;
+	}
+	public int getBgrade() {
+		return bgrade;
+	}
+	public void setBgrade(int bgrade) {
+		this.bgrade = bgrade;
+	}
+	public int getBview() {
+		return bview;
+	}
+	public void setBview(int bview) {
+		this.bview = bview;
+	}
+	public String getBthumb() {
+		return bthumb;
+	}
+	public void setBthumb(String bthumb) {
+		this.bthumb = bthumb;
+	}
+	public String getBinfo() {
+		return binfo;
+	}
+	public void setBinfo(String binfo) {
+		this.binfo = binfo;
+	}
+	
+	
 }
---
+++ khbooks/src/main/java/dto/SerialDTO.java
@@ -0,0 +1,72 @@
+package dto;
+
+import java.util.Date;
+
+public class SerialDTO {
+	int upno;
+	int bno;
+	Date sdate;
+	String scontent;
+	int sprice;
+	int ncount;
+	int auno;
+	String stitle;
+	int sgrade;
+	public int getUpno() {
+		return upno;
+	}
+	public void setUpno(int upno) {
+		this.upno = upno;
+	}
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public Date getSdate() {
+		return sdate;
+	}
+	public void setSdate(Date sdate) {
+		this.sdate = sdate;
+	}
+	public String getScontent() {
+		return scontent;
+	}
+	public void setScontent(String scontent) {
+		this.scontent = scontent;
+	}
+	public int getSprice() {
+		return sprice;
+	}
+	public void setSprice(int sprice) {
+		this.sprice = sprice;
+	}
+	public int getNcount() {
+		return ncount;
+	}
+	public void setNcount(int ncount) {
+		this.ncount = ncount;
+	}
+	public int getAuno() {
+		return auno;
+	}
+	public void setAuno(int auno) {
+		this.auno = auno;
+	}
+	public String getStitle() {
+		return stitle;
+	}
+	public void setStitle(String stitle) {
+		this.stitle = stitle;
+	}
+	public int getSgrade() {
+		return sgrade;
+	}
+	public void setSgrade(int sgrade) {
+		this.sgrade = sgrade;
+	}
+	
+	
+
+}
